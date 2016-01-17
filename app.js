6ee72a925e9763dcacbe753bf8b8369084ebc673
Srinivasan, Natraj(nsrinivasan) <nsrinivasan@ebay.com>
2016-01-17 17:08:10 -0500

    fix

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var app = express();
 
+
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port');
 });
 
 var Slack = require('slack-node');
 
-webhookUri = "__uri___";
+webhookUri = "https://hooks.slack.com/services/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";
 
 slack = new Slack();
 slack.setWebhook(webhookUri);
